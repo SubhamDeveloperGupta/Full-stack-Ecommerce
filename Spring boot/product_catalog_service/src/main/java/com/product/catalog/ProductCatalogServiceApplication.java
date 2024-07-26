d6273d369ae9ec90bf4b0fea3b4b69c4509074d4
Subham <subham7031979@gmail.com>
2024-07-26 09:26:54 +0530

    swagger config added

--- Spring boot/product_catalog_service/src/main/java/com/product/catalog/ProductCatalogServiceApplication.java
+++ Spring boot/product_catalog_service/src/main/java/com/product/catalog/ProductCatalogServiceApplication.java
@@ -1,9 +1,11 @@
 package com.product.catalog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class ProductCatalogServiceApplication {
 
 	public static void main(String[] args) {
