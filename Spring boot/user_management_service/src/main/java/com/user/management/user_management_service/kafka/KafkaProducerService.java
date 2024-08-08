43b72228085663a8b1bf970e46133fe86add7ad4
JavaCoderSubham <124339547+JavaCoderSubham@users.noreply.github.com>
2024-08-08 16:44:39 +0530

    kafka simple implementation done.

---
+++ Spring boot/user_management_service/src/main/java/com/user/management/user_management_service/kafka/KafkaProducerService.java
@@ -0,0 +1,37 @@
+package com.user.management.user_management_service.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public String sendMessageToTopic(String topicName, String message) {
+        try {
+            CompletableFuture<SendResult<String, Object>> resultKafka = kafkaTemplate.send(topicName, message);
+
+            resultKafka.whenComplete((t, ex) -> {
+                if (ex == null) {
+                    System.out.println("Result : " + message +
+                            "\nTopic : " + t.getRecordMetadata().topic() +
+                            "\nPartition : " + t.getRecordMetadata().partition() +
+                            "\nto Offset[" + t.getRecordMetadata().offset() + "]");
+                } else System.out.println("Exception : " + ex.getMessage());
+            });
+
+            return "Message Send Successfully.";
+        } catch (Exception ex) {
+            return "Kafka Server not working";
+        }
+    }
+
+}
