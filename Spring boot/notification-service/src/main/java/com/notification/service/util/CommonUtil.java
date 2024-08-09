086dc6e8eeb8201a1f5501bcbaaabc975c59b0b4
JavaCoderSubham <124339547+JavaCoderSubham@users.noreply.github.com>
2024-08-09 16:37:01 +0530

    Implement User Consumer in notification service

---
+++ Spring boot/notification-service/src/main/java/com/notification/service/util/CommonUtil.java
@@ -0,0 +1,8 @@
+package com.notification.service.util;
+
+public class CommonUtil {
+
+    public final static String GROUP_ID = "welcome-user";
+    public final static String USER_TOPIC = "Onboard_User_Mail";
+
+}
